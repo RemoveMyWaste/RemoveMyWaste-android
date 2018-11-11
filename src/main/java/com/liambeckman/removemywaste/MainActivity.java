3b34533fe5d1d727d0f16a49d76cd9d0afc43ceb
lbeckman314 <lbeckman314@gmail.com>
2018-11-10 18:52:33 -0800

    added main page, working on search materials

--- src/main/java/com/liambeckman/removemywaste/MainActivity.java
+++ src/main/java/com/liambeckman/removemywaste/MainActivity.java
@@ -11,3 +11,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+
+
+Button btn = (Button)findViewById(R.id.button4);
+
+btn.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(MainActivity.this, MainActivity.class));
+    }
+});
---
+++ src/main/java/com/liambeckman/removemywaste/search_material.java
@@ -0,0 +1,13 @@
+package com.liambeckman.removemywaste;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class search_material extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_material);
+    }
+}
